1fd127425ffa4bcfe1dbcb4b86f80a252bbec76a
knightza123 <knightza123@hotmail.com>
2018-02-20 03:52:44 +0700

    logo and navbar fixed css mobile first

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,11 +1,15 @@
 import React, {Component} from 'react';
-
+import logo from '../image/Kittinutlogo.png';
 class Navbar extends Component {
     render() {
         return (
             <header className='site-header'>
                 <div className='container-wide'>
-                    <h1 style={{color: "#fff"}}>Logohere</h1>
+                    <h1 className="logo">
+                        <a href="">
+                            <img src={logo}  alt=""/>
+                        </a>
+                    </h1>
                     <div className="site-header-panel">
                         <nav className="">
                             <ul>
--- src/container/Home.js
+++ src/container/Home.js
@@ -1,14 +1,25 @@
-import React,{Component} from 'react';
+import React, {Component} from 'react';
 import Navbar from '../component/Navbar';
-class Home extends Component{
-    render(){
+
+class Home extends Component {
+    render() {
         const date = new Date(2018, 2, 0).getDate();
 
-        return(
+        return (
             <div>
                 <Navbar/>
+                <div className="mobile-menu-trigger">
+                    <div>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+
+                </div>
+
             </div>
         )
     }
 }
+
 export default Home
